12216bb5de3100fc7251ef3684edf5eb9c265262
chuchunf <chuchunf@users.noreply.github.com>
2019-03-31 00:01:07 +0800

    text to speak

--- src/app/main/worddetail-component/worddetail-component.ts
+++ src/app/main/worddetail-component/worddetail-component.ts
@@ -1,16 +1,32 @@
-import { Component} from '@angular/core';
+import { Component } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
-    selector: 'app-main-worddetail',
-    templateUrl: './worddetail-component.html',
-    styleUrls: ['./worddetail-component.scss']
+  selector: 'app-main-worddetail',
+  templateUrl: './worddetail-component.html',
+  styleUrls: ['./worddetail-component.scss']
 })
 export class WordDetailComponent {
-    data = [
-        'Racing car sprays burning fuel into crowd.',
-        'Japanese princess to wed commoner.',
-        'Australian walks 100km after outback crash.',
-        'Man charged over missing wedding girl.',
-        'Los Angeles battles huge wildfires.'
-    ];
+  data = [
+    "Racing car sprays burning fuel into crowd.",
+    "Japanese princess to wed commoner.",
+    "Australian walks 100km after outback crash.",
+    "Man charged over missing wedding girl.",
+    "Los Angeles battles huge wildfires."
+  ];
+
+  constructor(private router: Router) {}
+
+  public clickClose() {
+    this.router.navigate(['/home/list']);
+  }
+
+  public clickSpeak() {
+    if (window.speechSynthesis) {
+      const msg = new SpeechSynthesisUtterance();
+      msg.rate = 0.6;
+      msg.text = 'module';
+      speechSynthesis.speak(msg);
+    }
+  }
 }
